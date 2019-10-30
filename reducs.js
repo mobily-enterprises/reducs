61d6b3fad1e2727dd702c8e83c3a47cc0f852f04
Tony Mobily <tonymobily@gmail.com>
2019-10-30 10:31:43 +0800

    Added testing code

--- reducs.js
+++ reducs.js
@@ -1,7 +1,7 @@
 const state = {}
 let beforeCallbacks = []
 let afterCallbacks = []
-let globalId = 10
+let globalId = 0
 
 const deleteId = (id, type) => {
   type === 'a'
@@ -21,7 +21,30 @@ export const setState = async (key, value) => {
     if (v instanceof Promise) await v
   }
 }
-
 export const stateEmpty = () => !Object.keys(state).length
 export const getState = (key) => state[key]
+/*
+// debugger
 
+setState('state', 1)
+const f1 = () => console.log('f1')
+const f2 = () => console.log('f2')
+const f3 = () => console.log('f3')
+
+const removeF11 = register(f1)
+const removeF12 = register(f1)
+const removeF2 = register(f2)
+const removeF3 = register(f3, 'b')
+
+setState('state', 2)
+console.log('-------')
+
+removeF2()
+
+setState('state', 3)
+console.log('-------')
+
+removeF3()
+setState('state', 4)
+console.log('-------')
+*/
